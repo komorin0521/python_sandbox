fcf36c03378a4099cb1086e99806e8a168ae76a5
komorin0521 <haikaradaisuki@gmail.com>
2017-10-14 00:01:39 +0900

    add ntpclient

---
+++ python3/ntpclient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import ntplib
+from time import ctime
+import datetime
+
+__author__ = "oomori"
+__version__ = "1.0.0"
+
+ntp_client = ntplib.NTPClient()
+res = ntp_client.request('ntp.nict.go.jp')
+nowtime = datetime.datetime.strptime(ctime(res.tx_time), "%a %b %d %H:%M:%S %Y")
+
+print(nowtime.strftime("%Y/%m/%d %H:%M:%S"))
+
