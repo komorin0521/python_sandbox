117c3e291fbf30a2cc52d013300caa2c2672b404
komorin0521 <haikaradaisuki@gmail.com>
2017-10-14 00:17:07 +0900

    modify ntp server address

--- python3/ntpclient.py
+++ python3/ntpclient.py
@@ -8,7 +8,7 @@ __author__ = "oomori"
 __version__ = "1.0.0"
 
 ntp_client = ntplib.NTPClient()
-res = ntp_client.request('ntp.nict.go.jp')
+res = ntp_client.request('ntp.nict.jp')
 nowtime = datetime.datetime.strptime(ctime(res.tx_time), "%a %b %d %H:%M:%S %Y")
 
 print(nowtime.strftime("%Y/%m/%d %H:%M:%S"))
