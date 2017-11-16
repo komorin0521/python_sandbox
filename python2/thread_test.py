5e99df7771f1da5c898b1eed0c4e943b30502632
komorin0521 <haikaradaisuki@gmail.com>
2017-11-16 21:56:30 +0900

    modify gets results of each thread

--- python2/thread_test.py
+++ python2/thread_test.py
@@ -6,7 +6,7 @@ import time
 __author__ = "oomori"
 __version__ = "1.0.0"
 
-def myfunc(test_arg):
+def myfunc(test_arg, results):
     """
     This is a test function of multithreading
     :param test_arg
@@ -17,6 +17,7 @@ def myfunc(test_arg):
     print(test_arg)
     output = test_arg * 2
     print("output is %d" % output)
+    results[threading.current_thread().name] = output
 
 def main():
     """
@@ -24,17 +25,24 @@ def main():
     :return: None
     """
 
+    results = dict()
     for _  in range(0, 10):
         threadlist = list()
         for thread_num in range(0, 5):
-            thread = threading.Thread(target=myfunc, args=([thread_num]), 
+            thread = threading.Thread(target=myfunc, args=([thread_num, results]), 
                     name="thread%d" % thread_num)
             threadlist.append(thread)
 
         for thread in threadlist:
             thread.start()
-        time.sleep(5)
 
+        for thread in threadlist:
+            thread.join()
+
+        print("=== show each thread results ===")
+        for k in results:
+            print(k, results[k])
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
